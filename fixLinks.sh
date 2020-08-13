81c55d3585fd8259c35bc47e925e69b3b3c4893d
James <s3656070@student.rmit.edu.au>
2020-08-14 00:17:33 +1000

    fixes

--- fixLinks.sh
+++ fixLinks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rootURL="https://jameswrc.github.io/"
 keyWord="assets/"
-sed -i -e 's,'"${keyWord}"','"${rootURL}${keyWord}"',g' index.html
+dir="/Users/james/Documents/GitHub/Sites/JamesWRC.github.io/"
+sed -i -e 's,'"${keyWord}"','"${rootURL}${keyWord}"',g' "${dir}index.html"
