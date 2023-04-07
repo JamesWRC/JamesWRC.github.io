80885608a9d51c7ae1359f240aca207832472436
JamesWRC <james.cockshott@hotmail.com>
2023-04-07 13:29:43 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-async function handleRequest() {
+async function handleRequest(params) {
+  var a = "no value"
+  // try{
+  // await TEST.put("a", "this is a message")
+  // a = await TEST.get("a'")
+  // } catch (err){
+  //   a = err
+  // }
   const init = {
     headers: {
       'content-type': 'text/html;charset=UTF-8',
+      'text':String(a)
     },
   }
+  const url = 'https://jameswrc.github.io' + params
+
   const response = await fetch(url, init)
   const results = await gatherResponse(response)
   return new Response(results, init)
 }
 addEventListener('fetch', event => {
-  return event.respondWith(handleRequest())
+  var uri = event.request.url.replace(/^https:\/\/.*?\//gi, "/");
+  let params = uri
+  return event.respondWith(handleRequest(params))
 })
 /**
  * gatherResponse awaits and returns a response body as a string.
@@ -33,4 +45,3 @@ async function gatherResponse(response) {
  * Example someHost at url is set up to respond with HTML
  * Replace url with the host you wish to send requests to
  */
-const url = 'https://jameswrc.github.io'
